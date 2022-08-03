0f2e611cfbe0f56dc0f8b4dacdf53d4787fc701c
Akash Bag <78099533+Akashbag2001@users.noreply.github.com>
2022-08-03 21:50:32 +0530

    Update Number of 1 Bits.cpp

--- Number of 1 Bits.cpp
+++ Number of 1 Bits.cpp
@@ -12,3 +12,17 @@ public:
             return  count;
     }
 };
+// fastest approach
+class Solution {
+public:
+   int hammingWeight(uint32_t n) {
+    int count = 0;
+    
+    while (n) {
+        n &= (n - 1);
+        count++;
+    }
+    
+    return count;
+}
+};
