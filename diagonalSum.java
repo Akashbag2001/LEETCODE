5e7ec9b0828de914fc85033d719a3e20ddbd4df9
Akash Bag <78099533+Akashbag2001@users.noreply.github.com>
2022-09-04 21:32:38 +0530

    Create diagonalSum.java

---
+++ diagonalSum.java
@@ -0,0 +1,34 @@
+public class diagonalSum {
+    public static int solution(int matrix[][]){
+        int sum =0;
+        // brute force
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         // for primary diagonal 
+        //         if(i == j){
+        //             sum += matrix[i][j];
+        //         }
+        //         // for secondary diagonal 
+        //         else if(i+j == matrix.length-1){
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+        for(int i=0;i<matrix.length;i++){
+            sum += matrix[i][i];
+            if(i != matrix.length -1-i){
+                sum += matrix[i][matrix.length -1-i];
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int matrix[][] ={{1,2,3,4},
+        { 5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16}
+       };
+
+       System.out.println(solution(matrix));;
+    }
+}
